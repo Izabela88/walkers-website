123e7f9071fc9dc9ab1c2332fcc9e664b9078d7d
Izabela88 <izabkru88@gmail.com>
2022-02-10 20:10:45 +0000

    Remove phone field from registration form, add context to view.py in home and user profile files

--- home/views.py
+++ home/views.py
@@ -7,26 +7,26 @@ from home.forms import PetsitterQuestion, FormValidationError
 
 # Create your views here.
 def index(request):
-    # context = {}
-    # if request.user.is_authenticated:
-    #     if request.user.is_petsitter is None:
-    #         return redirect('/question') 
-    #     context['is_petsitter'] = request.user.is_petsitter
-    return render(request, 'home/index.html', )
+    context = {}
+    if request.user.is_authenticated:
+        if request.user.is_petsitter is None:
+            return redirect('/question') 
+        context['is_petsitter'] = request.user.is_petsitter
+    return render(request, 'home/index.html', context)
 
 
 
-# @login_required
-# def register_question(request):
-#     if request.method == "GET":
-#         form_petsitter = PetsitterQuestion()
-#         return render(request, 'home/question.html', {'form_petsitter': form_petsitter})
-#     if request.method == "POST":
-#         pet_sitter = PetsitterQuestion(request.POST)
-#         try:
-#             pet_sitter.save(request.user)
-#         except FormValidationError:
-#             redirect('/question')
-#         return redirect("/")
+@login_required
+def register_question(request):
+    if request.method == "GET":
+        form_petsitter = PetsitterQuestion()
+        return render(request, 'home/question.html', {'form_petsitter': form_petsitter})
+    if request.method == "POST":
+        pet_sitter = PetsitterQuestion(request.POST)
+        try:
+            pet_sitter.save(request.user)
+        except FormValidationError:
+            redirect('/question')
+        return redirect("/")
 
 
--- walker_profile/views.py
+++ walker_profile/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def user_profile(request):
-    return render(request, 'user_profile/user_profile.html')
+    context = {}
+    if request.user.is_authenticated:
+        context['is_petsitter'] = request.user.is_petsitter
+    return render(request, 'user_profile/user_profile.html', context)
