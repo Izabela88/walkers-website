40e9ccc5395c43f177a1cf59a2ec3a87f2a0e700
Izabela88 <izabkru88@gmail.com>
2022-02-11 17:19:51 +0000

    Restyle login and register pages

--- walker_profile/models.py
+++ walker_profile/models.py
@@ -1,10 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+# from django.db.models.signals import post_save
+# from django.dispatch import receiver
 
 
 class WalkerUser(AbstractUser):
     is_petsitter = models.BooleanField(null=True)
-    phone_number = models.CharField(max_length=20, null=True)
+    username = models.CharField(max_length=150, null=True, unique=False)
+
+    def __str__(self):
+        return str(self.username)
 
 
+# @receiver(post_save, sender=WalkerUser)
+# def create_profile(sender, instance, created, **kwargs):
+#     if created:
+#         WalkerUser.objects.create(user=instance)
 
+# @receiver(post_save, sender=WalkerUser)
+# def save_profile(sender, instance, **kwargs):
+#     instance.user.save()
