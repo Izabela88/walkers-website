3a6217107b460ba8f75519df7c6821a2893905bb
Izabela88 <izabkru88@gmail.com>
2022-03-28 10:30:42 +0100

    Format settings py file

--- walkers/settings.py
+++ walkers/settings.py
@@ -247,7 +247,6 @@ else:
     DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
