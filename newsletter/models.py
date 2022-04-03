6f3ff6891552cf0e6c4f6efd488a39d8480c1488
Izabela88 <izabkru88@gmail.com>
2022-04-03 11:54:42 +0100

    Newsletter app formatting and code refactoring

--- newsletter/models.py
+++ newsletter/models.py
@@ -6,3 +6,9 @@ class NewsletterUser(models.Model):
     is_subscribed = models.BooleanField(null=False, default=True)
     created_at = models.DateTimeField(auto_now_add=True, null=False)
     unsubscribed_at = models.DateTimeField(null=True)
+
+    def subscribe(self):
+        """Subscribe user"""
+        if self.is_subscribed is False:
+            self.is_subscribed = True
+            self.save()
--- newsletter/views.py
+++ newsletter/views.py
@@ -1,62 +1,27 @@
 from django.views import View
 from newsletter.forms import NewsletterUserForm
 from newsletter.models import NewsletterUser
-from django.conf import settings
-from mailchimp_marketing import Client
-from mailchimp_marketing.api_client import ApiClientError
+from newsletter.mailchimp_utils import subscribe
 from django.contrib import messages
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 import datetime
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from newsletter.serializers import SubscriptionSerializer
+from django.urls import reverse
 from django.shortcuts import get_object_or_404
-
-# Mailchimp Settings
-api_key = settings.MAILCHIMP_API_KEY
-server = settings.MAILCHIMP_DATA_CENTER
-list_id = settings.MAILCHIMP_EMAIL_LIST_ID
-
-
-def subscribe(email):
-    """
-    Contains code handling the communication to the mailchimp api
-    to create a contact/member in an audience/list.
-    """
-
-    mailchimp = Client()
-    mailchimp.set_config(
-        {
-            "api_key": api_key,
-            "server": server,
-        }
-    )
-
-    member_info = {
-        "email_address": email,
-        "status": "subscribed",
-    }
-
-    try:
-        response = mailchimp.lists.add_list_member(list_id, member_info)
-        print("response: {}".format(response))
-    except ApiClientError as error:
-        print("An exception occurred: {}".format(error.text))
 
 
 class Newsletter(View):
-    def post(self, request):
+    def post(self, request) -> HttpResponse:
+        """Subscribe email address"""
         email_form = NewsletterUserForm(data=request.POST or None)
         if email_form.is_valid():
             email = email_form.cleaned_data['newsletter_email']
             newsletter = NewsletterUser.objects.filter(email=email).first()
             if newsletter:
-                if not newsletter.is_subscribed:
-                    newsletter.is_subscribed = True
-                    newsletter.save()
-                subscribe(newsletter.email)
-
+                newsletter.subscribe()
             else:
                 data = NewsletterUser()
                 data.email = email
@@ -66,25 +31,25 @@ class Newsletter(View):
             messages.success(
                 request, "Thank You for subscribe to our newsletter!"
             )
-            return HttpResponseRedirect('/')
+            return HttpResponseRedirect(reverse('home'))
         else:
             messages.error(request, 'Something went wrong!')
 
-        return HttpResponseRedirect('/')
+        return HttpResponseRedirect(reverse('home'))
 
 
 class UpdateSubscription(APIView):
     serializer_class = SubscriptionSerializer
 
-    def get(self, request):
+    def get(self, _) -> Response:
         """
-        ping method
+        Ping method. Required by mailchimp.
         """
         return Response({}, status=status.HTTP_200_OK)
 
-    def post(self, request):
+    def post(self, request) -> Response:
         """
-        Update subscription
+        Update subscription webhook
         """
 
         serializer = self.serializer_class(data=request.data)
