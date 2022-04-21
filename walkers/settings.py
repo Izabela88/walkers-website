e3027f5ef9bf8964cc8ac9db4e682defa9fb2c8c
Izabela88 <izabkru88@gmail.com>
2022-04-21 15:22:08 +0100

    Google auth

--- walkers/settings.py
+++ walkers/settings.py
@@ -128,9 +128,15 @@ SOCIALACCOUNT_PROVIDERS = {
         "SCOPE": [
             "profile",
             "email",
+            "https://www.googleapis.com/auth/userinfo.profile",
+            "https://www.googleapis.com/auth/userinfo.email",
+            "openid",
         ],
         "AUTH_PARAMS": {
             "access_type": "online",
+            "response_type": "code",
+            "redirect_uri": "https://walkers88.herokuapp.com/accounts/google/login/callback/",
+            "client_id": os.environ.get("CLIENT_ID"),
         },
     },
 }
