a4be42dbe30b0b49b763d345d7159f26045802f4
Izabela Krupinska <54544131+Izabela88@users.noreply.github.com>
2023-02-14 17:26:37 +0000

    Update settings.py

--- walkers/settings.py
+++ walkers/settings.py
@@ -37,7 +37,7 @@ if os.getenv("ENV") == "PRODUCTION":
 else:
     DEBUG = True
 
-ALLOWED_HOSTS = ["walkers88.herokuapp.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 ROOT_URLCONF = "walkers.urls"
 
@@ -191,17 +191,17 @@ WSGI_APPLICATION = "walkers.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if os.getenv("ENV") == "PRODUCTION":
-    DATABASES = {
-        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
-    }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+# if os.getenv("ENV") == "PRODUCTION":
+DATABASES = {
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
+# else:
+#     DATABASES = {
+#         "default": {
+#             "ENGINE": "django.db.backends.sqlite3",
+#             "NAME": BASE_DIR / "db.sqlite3",
+#         }
+#     }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
